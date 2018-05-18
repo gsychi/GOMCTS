f32d607b32da3ed25f7c9fa63473a9788c45c62b
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2018-05-18 09:44:40 -0400

    Update TTTEnvironment.py

--- TTTEnvironment.py
+++ TTTEnvironment.py
@@ -11,8 +11,8 @@ def stateToNumber(gameState):
     temp=gameState[0],gameState[1],gameState[2]
     number="" #19 places- 9 for X, 9 for O, 1 for turn
     for x in range(18):
-        matrix=x%9 #Tells whether to look at matrix X or matrix O
-        x=int((x-x%9)/2)
+        #Tells whether to look at matrix X or matrix O
+        matrix=int((x-(x%9))/9)
         column=x%3 #Tells you the column of the array
         row=int((x-column)/3)%3 #Tells you the row of the array
         number=number+str(int(temp[matrix][row,column]))
@@ -20,15 +20,23 @@ def stateToNumber(gameState):
     return int(number)
 
 def numberToState(number):
+    length=int(np.log10(number+1))+1
     number=str(number)
-    stateX=np.array(3,3)
-    stateO=np.array(3,3)
-    Turn=0
-    gameState=stateX, stateO, turn
+    for x in range(19-length):
+        number='0'+number
+    number=str(number)
+    stateX=np.zeros((3,3))
+    stateO=np.zeros((3,3))
+    turn=0
     for x in range(18):
         matrix=x%9 #Tells whether to look at matrix X or matrix O
-        y=x-9*int(x/9)
-        column=y%3 #Tells you the column of the array
-        row=int((y-column)/3) #Tells you the row of the array
-        gameState[y][row,column]=number[x]
-    gameState[2]=number[-1]
+        matrix = int((x - (x % 9)) / 9)
+        column = x % 3  # Tells you the column of the array
+        row = int((x - column) / 3) % 3  # Tells you the row of the array
+        if(matrix==0):
+            stateX[row, column] = number[x]
+        if(matrix==1):
+            stateO[row, column] = number[x]
+    turn=number[-1]
+    gameState = stateX, stateO, turn
+    return(gameState)
