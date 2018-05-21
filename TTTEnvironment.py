edfebe6a3a7e5a4410f0b44578c1067ff8408fc0
Benw8888 <39271586+Benw8888@users.noreply.github.com>
2018-05-21 12:09:08 -0400

    Update TTTEnvironment.py

--- TTTEnvironment.py
+++ TTTEnvironment.py
@@ -10,6 +10,9 @@ class TTTEnvironment:
         self.turn = 0  # 0 for X to move, 1 for O to move
         self.state = self.Xstate, self.Ostate, self.turn
 
+    def setValues(self):
+        self.Xstate, self.Ostate, self.turn=self.state
+
     def stateToNumber(gameState):
         temp = gameState[0], gameState[1], gameState[2]
         number = ""  # 19 places- 9 for X, 9 for O, 1 for turn
@@ -22,9 +25,42 @@ class TTTEnvironment:
         number = number + str(int(temp[2]))
         return int(number)
 
+    def stateToString(gameState):
+        temp = gameState[0], gameState[1], gameState[2]
+        number = ""  # 19 places- 9 for X, 9 for O, 1 for turn
+        for x in range(18):
+            # Tells whether to look at matrix X or matrix O
+            matrix = int((x - (x % 9)) / 9)
+            column = x % 3  # Tells you the column of the array
+            row = int((x - column) / 3) % 3  # Tells you the row of the array
+            number = number + str(int(temp[matrix][row, column]))
+        number = number + str(int(temp[2]))
+        return number
+
     def numberToState(number):
         length = int(np.log10(number + 1)) + 1
         number = str(number)
+        for x in range(19 - length):
+            number = '0' + number
+        number = str(number)
+        stateX = np.ndarray((3, 3))
+        stateO = np.ndarray((3, 3))
+        turn = 0
+        for x in range(18):
+            matrix = x % 9  # Tells whether to look at matrix X or matrix O
+            matrix = int((x - (x % 9)) / 9)
+            column = x % 3  # Tells you the column of the array
+            row = int((x - column) / 3) % 3  # Tells you the row of the array
+            if (matrix == 0):
+                stateX[row, column] = number[x]
+            if (matrix == 1):
+                stateO[row, column] = number[x]
+        turn = number[-1]
+        gameState = stateX, stateO, turn
+        return (gameState)
+
+    def stringToState(string):
+        length = len(string)
         for x in range(19 - length):
             number = '0' + number
         number = str(number)
