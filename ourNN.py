a0c515739427de17d9183a21b06999828608b29b
chanseo07 <baebrian07@gmail.com>
2018-05-23 20:45:24 -0400

    Go env + added layer to NN

--- ourNN.py
+++ ourNN.py
@@ -19,10 +19,14 @@ class NeuralNetwork:
         # define weight and bias for input -> hidden and hidden -> input
         self.weight_1 = np.random.uniform(-1.0, 1.0, (len(self.X[0]), self.hiddenNodes))
         self.weight_2 = np.random.uniform(-1.0, 1.0, (self.hiddenNodes, self.hiddenNodes))
-        self.weight_3 = np.random.uniform(-1.0, 1.0, (self.hiddenNodes, len(self.y[0])))
+        self.weight_3 = np.random.uniform(-1.0, 1.0, (self.hiddenNodes, self.hiddenNodes))
+        self.weight_4 = np.random.uniform(-1.0, 1.0, (self.hiddenNodes, self.hiddenNodes))
+        self.weight_5 = np.random.uniform(-1.0, 1.0, (self.hiddenNodes, len(self.y[0])))
         self.bias_1 = np.random.random((1, self.hiddenNodes))
         self.bias_2 = np.random.random((1, self.hiddenNodes))
-        self.bias_3 = np.random.random((1, len(self.y[0])))
+        self.bias_3 = np.random.random((1, self.hiddenNodes))
+        self.bias_4 = np.random.random((1, self.hiddenNodes))
+        self.bias_5 = np.random.random((1, len(self.y[0])))
 
     def trainNetwork(self, val, alpha):
 
@@ -34,18 +38,29 @@ class NeuralNetwork:
             b = inp.dot(self.weight_2)+self.bias_2
             hid = 1 / (1 + np.exp(-b))
             c = hid.dot(self.weight_3)+self.bias_3
-            fin = 1 / (1 + np.exp(-c))
+            hid2 = 1 / (1 + np.exp(-c))
+            d = hid2.dot(self.weight_4)+self.bias_4
+            hid3 = 1 / (1 + np.exp(-d))
+            e = hid3.dot(self.weight_5)+self.bias_5
+            fin = 1 / (1 + np.exp(-e))
 
             #backward propagation
             fin_delta = (self.y - fin) * (fin * (1 - fin))
-            hid_delta = fin_delta.dot(self.weight_3.T) * (hid * (1 - hid))
+            hid3_delta = fin_delta.dot(self.weight_5.T) * (hid3 * (1 - hid3))
+            hid2_delta = hid3_delta.dot(self.weight_4.T) * (hid2 * (1 - hid2))
+            hid_delta = hid2_delta.dot(self.weight_3.T) * (hid * (1 - hid))
             inp_delta = hid_delta.dot(self.weight_2.T) * (inp * (1 - inp))
 
-            self.weight_3 += (fin_delta.T.dot(hid)).T * alpha
+
+            self.weight_5 += (fin_delta.T.dot(hid3)).T * alpha
+            self.weight_4 += (hid3_delta.T.dot(hid2)).T * alpha
+            self.weight_3 += (hid2_delta.T.dot(hid)).T * alpha
             self.weight_2 += (hid_delta.T.dot(inp)).T * alpha
             self.weight_1 += (inp_delta.T.dot(self.X)).T * alpha
 
-            self.bias_3 += fin_delta.sum(axis=0) * alpha
+            self.bias_5 += fin_delta.sum(axis=0) * alpha
+            self.bias_4 += hid3_delta.sum(axis=0) * alpha
+            self.bias_3 += hid2_delta.sum(axis=0) * alpha
             self.bias_2 += hid_delta.sum(axis=0) * alpha
             self.bias_1 += inp_delta.sum(axis=0) * alpha
 
@@ -53,7 +68,9 @@ class NeuralNetwork:
     def predict(self, input):
         updated_inp = 1 / (1 + np.exp(-(np.dot(input, self.weight_1)+self.bias_1)))
         updated_hid = 1 / (1 + np.exp(-(np.dot(updated_inp, self.weight_2)+self.bias_2)))
-        updated_fin = 1 / (1 + np.exp(-(np.dot(updated_hid, self.weight_3)+self.bias_3)))
+        updated_hid2 = 1 / (1 + np.exp(-(np.dot(updated_hid, self.weight_3) + self.bias_3)))
+        updated_hid3 = 1 / (1 + np.exp(-(np.dot(updated_hid2, self.weight_4) + self.bias_4)))
+        updated_fin = 1 / (1 + np.exp(-(np.dot(updated_hid3, self.weight_5)+self.bias_5)))
 
         return updated_fin
 
